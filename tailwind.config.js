2f7554c1f369ba2a3059042d9aaa7a33c8d01edd
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-05-09 10:16:55 +0200

    update config to respect new locations

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
    * files that contain Tailwind class names.
    * https://tailwindcss.com/docs/content-configuration
    */
-  content: ["./src/index.ejs", "./src/sections/*.ejs"],
+  content: ["./src/index.ejs", "./src/sections/**/*.ejs"],
 
   /**
    * The theme section is where you define your color palette, 
