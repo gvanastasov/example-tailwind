7a07d529c6027ccbb5c5669d510f9881d909ace3
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-05-21 21:15:26 +0200

    add section url resolver

--- server.js
+++ server.js
@@ -1,78 +1,44 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const livereload = require("livereload");
-const connectLivereload = require("connect-livereload");
+/** imports */
+const express         = require("express"),
+  path                = require("path"),
+  fs                  = require("fs"),
+  livereload          = require("livereload"),
+  connectLivereload   = require("connect-livereload");
 
-const server = express();
-const hostname = "localhost";
-const port = process.env.PORT || 8080;
-const publicLocation = express.static(path.join(__dirname, "./public"));
-const entryTemplate = path.join(__dirname, "./src/index.ejs");
+/** locals */
+const server          = express(),
+  hostname            = "localhost",
+  port                = process.env.PORT || 8080,
+  publicLocation      = express.static(path.join(__dirname, "./public")),
+  entryTemplate       = path.join(__dirname, "./src/index.ejs");
 
+/**
+ * @description hardcoded route table. See below catch all endpoint
+ * for further details on why this is done.
+ */
 let sectionTemplates = [
-  {
-    name: "Layout",
-    path: "/layout",
-  },
-  {
-    name: "Flexbox",
-    path: "/flexbox",
-  },
-  {
-    name: "Grid",
-    path: "/grid",
-  },
-  {
-    name: "Spacing",
-    path: "/spacing",
-  },
-  {
-    name: "Sizing",
-    path: "/sizing",
-  },
-  {
-    name: "Typography",
-    path: "/typography",
-  },
-  {
-    name: "Background",
-    path: "/background",
-  },
-  {
-    name: "Border",
-    path: "/border",
-  },
-  {
-    name: "Effects",
-    path: "/effects"
-  },
-  {
-    name: "Filters",
-    path: "/filters"
-  },
-  {
-    name: "Tables",
-    path: "/tables"
-  },
-  {
-    name: "Transitions",
-    path: "/transitions"
-  },
-  {
-    name: "Transforms",
-    path: "/transforms"
-  },
-  {
-    name: "Interactivity",
-    path: "/interactivity"
-  },
-  {
-    name: "SVG",
-    path: "/svg"
-  }
+  { name: "Layout",         path: "/layout" },
+  { name: "Flexbox",        path: "/flexbox" },
+  { name: "Grid",           path: "/grid" },
+  { name: "Spacing",        path: "/spacing" },
+  { name: "Sizing",         path: "/sizing" },
+  { name: "Typography",     path: "/typography" },
+  { name: "Background",     path: "/background" },
+  { name: "Border",         path: "/border" },
+  { name: "Effects",        path: "/effects" },
+  { name: "Filters",        path: "/filters" },
+  { name: "Tables",         path: "/tables" },
+  { name: "Transitions",    path: "/transitions" },
+  { name: "Transforms",     path: "/transforms" },
+  { name: "Interactivity",  path: "/interactivity" },
+  { name: "SVG",            path: "/svg" }
 ].map(x => ({...x, sections: []}));
 
+/**
+ * @description a very dummy scanning and in-memory cache
+ * of locations of templates to be passed to .ejs render
+ * functions when including markup on the page being served.
+ */
 const scan = (location) => {
   const result = [];
   const sectionTemplatesPath = path.join(
@@ -125,8 +91,32 @@ if (process.env.NODE_ENV === "development") {
   server.use(connectLivereload());
 }
 
-server.get("/", function (_req, res) {
-  res.render(entryTemplate, { sections: sectionTemplates });
+/**
+ * @description a pretty hacky solution for template resolution based
+ * on hardcoded route table from above. This can optimized/rewritten in 
+ * many different ways and is not the subject of this demo anyhow.
+ */
+server.get("/*", function (req, res, next) {
+  if (req.url === "/") {
+    console.log('route home')
+    res.render(entryTemplate, { sections: sectionTemplates, current: null });
+  } else {
+    let route = undefined
+    
+    sectionTemplates.forEach(x => {
+      x.sections.forEach(s => {
+       if (`${x.path}/${s}` === req.url) {
+        route = `${x.path}/${s}`
+       }
+      })
+    });
+
+    if (route) {
+      res.render(entryTemplate, { sections: sectionTemplates, current: route });
+    } else {
+      return next();
+    }
+  }
 });
 
 server.use(publicLocation);
